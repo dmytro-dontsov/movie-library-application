27d65a7e9dd5bb39b3e70b46b898c418dae9d8b6
dmytro-dontsov <qemmnes@gmail.com>
2024-10-06 19:18:31 +0200

    Connection BD mySQL

--- src/main/java/com/example/movielibrary/MovieController.java
+++ src/main/java/com/example/movielibrary/MovieController.java
@@ -1,13 +1,24 @@
 package com.example.movielibrary;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
 
+    @Autowired
+    MovieRepositry movieRepositry;
+
     @GetMapping("/test")
     public int test() {
         return 1;
+    }
+
+    @GetMapping("/movies")
+    public List<Movie> getAll() {
+        return movieRepositry.getAll();
     }
 }
